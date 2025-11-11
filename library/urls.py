c1b7d952e0858e0d4d944021ad01ec4049a21ee9
CodingLL25 <6b45ofgn@students.codeinstitute.net>
2025-11-11 20:23:12 +0000

    Add delete collection and add book

--- library/urls.py
+++ library/urls.py
@@ -1,9 +1,17 @@
 from . import views
+from .views import delete_collection, add_book_to_collection
+
 from django.urls import path, include
 
 urlpatterns = [
     path("", views.CollectionList.as_view(), name="home"),
     path("accounts/", include("allauth.urls")),
     path("create/", views.create_collection, name="create_collection"),
+    path(
+        "collection/<int:collection_id>/add-book/",
+        add_book_to_collection,
+        name="add_book",
+    ),
+    path("collection/<slug:slug>/delete/", delete_collection, name="collection_delete"),
     path("<slug:slug>/", views.collection_detail, name="collection_detail"),
 ]
