8331500b9bc6c17f33917a5b5cea15ec6a2ebd7b
CodingLL25 <6b45ofgn@students.codeinstitute.net>
2025-11-11 18:58:33 +0000

    Add authentication

--- library/urls.py
+++ library/urls.py
@@ -1,8 +1,9 @@
 from . import views
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path("", views.CollectionList.as_view(), name="home"),
+    path("accounts/", include("allauth.urls")),
     path("create/", views.create_collection, name="create_collection"),
     path("<slug:slug>/", views.collection_detail, name="collection_detail"),
 ]
