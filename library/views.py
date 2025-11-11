fe61d71978a80f02e7a2ed9c51202d83c57609ca
CodingLL25 <6b45ofgn@students.codeinstitute.net>
2025-11-11 19:59:30 +0000

    Amend index.html format to show form after example collections

--- library/views.py
+++ library/views.py
@@ -12,7 +12,7 @@ class CollectionList(generic.ListView):
 
     queryset = Collection.objects.all().order_by("id")
     template_name = "library/index.html"
-    paginate_by = 2
+    paginate_by = 3
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
