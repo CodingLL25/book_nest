a6c0a8c028461686380d63a7abc6d880b01ab0d5
CodingLL25 <6b45ofgn@students.codeinstitute.net>
2025-12-02 19:07:34 +0000

    Add docstrings

--- library/apps.py
+++ library/apps.py
@@ -2,5 +2,8 @@ from django.apps import AppConfig
 
 
 class LibraryConfig(AppConfig):
+    """
+    Application configuration for the 'library' app.
+    """
     default_auto_field = "django.db.models.BigAutoField"
     name = "library"
