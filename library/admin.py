662eadab2355f24bdf340d7d1ef1941fea82ffa0
CodingLL25 <6b45ofgn@students.codeinstitute.net>
2025-12-02 19:12:24 +0000

    Add docstrings and amend code greater than 79 characters

--- library/admin.py
+++ library/admin.py
@@ -10,21 +10,29 @@ def clean_html(text):
 
 
 class TagFilter(SimpleListFilter):
+    """
+    Custom list filter for the Django admin interface.
+
+    This filter adds a dropdown in the Book admin that allows
+    filtering books by their associated tags.
+    """
     title = "Tag"  # name of the filter
     parameter_name = "tag"
 
-    def lookups(self, request, model_admin):  # populate dropdown filter options
-        return [(tag.id, tag.name) for tag in Tag.objects.all()]  # as tuples
+    def lookups(self, request, model_admin):
+        return [(tag.id, tag.name) for tag in Tag.objects.all()]
 
-    def queryset(self, request, queryset):  # filtering logic
-        if self.value():  # return selected tag ID
+    def queryset(self, request, queryset):
+        if self.value():
             return queryset.filter(books__tag__id=self.value()).distinct()
         return queryset
 
 
 @admin.register(Collection)
 class Collection(SummernoteModelAdmin):
-
+    """
+    Custom Django admin configuration for the Collection model.
+    """
     list_display = ("name", "theme", "excerpt_clean")
     search_fields = ["theme", "excerpt"]
     list_filter = (TagFilter, "theme")
@@ -38,7 +46,9 @@ class Collection(SummernoteModelAdmin):
 # error with filtering the book by tag
 @admin.register(Book)
 class Book(SummernoteModelAdmin):
-
+    """
+    Custom Django admin configuration for the Book model.
+    """
     list_display = ("title", "author", "body_clean")
     search_fields = ["title", "author"]
     list_filter = (TagFilter, "author")
